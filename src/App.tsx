7ae71dfc49fa8a4100b117bde65cadc23894178a
Edson Cavalcante de Albuquerque Bisneto <51378539+edsonkz@users.noreply.github.com>
2022-11-19 12:32:39 -0300

    Finished create user page and added login page.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<SignUp />} />
+				<Route path="/login" element={<SignIn />} />
 			</Routes>
 		</Router>
 	);
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,46 +1,12 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../styles/SignUp.css";
 
 function SignUp() {
-	const navigate = useNavigate();
 	const [notification, setNotification] = useState("");
 	const [error, setError] = useState(false);
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
-
-	const addUser = (event: React.FormEvent) => {
-		event.preventDefault();
-
-		axios
-			.post("http://localhost:3333/users", { username, password })
-			.then(() => {
-				console.log("Usuário criado.");
-				setUsername("");
-				setPassword("");
-				setNotification("Usuário criado com sucesso.");
-				setError(false);
-
-				setTimeout(() => {
-					navigate("/login");
-				}, 2000);
-			})
-			.catch((e) => {
-				console.log(e.response.data);
-				if (e.response.data.error) {
-					setNotification(
-						"Erro de validação. A senha precisa ter no mínimo 8 caracteres, 1 caractere numérico e 1 caractere em letra maiúscula."
-					);
-					setError(true);
-				}
-				if (e.response.data.erroCode === "23505")
-					setNotification(
-						"Nome de usuário já existente, tente outro."
-					);
-				setError(true);
-			});
-	};
 
 	return (
 		<div className="main">
@@ -52,8 +18,8 @@ function SignUp() {
 				) : (
 					<h5 className="error">{notification}</h5>
 				)}
-				<h2>Crie seu usuário</h2>
-				<form className="form_sign_up" onSubmit={addUser}>
+				<h2>Entrar</h2>
+				<form className="form_sign_up">
 					<input
 						type="text"
 						className="username_input"
@@ -71,7 +37,7 @@ function SignUp() {
 						required
 					></input>
 
-					<button>Criar</button>
+					<button>Entrar</button>
 				</form>
 			</div>
 		</div>
